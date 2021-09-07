297ef91e051ebab454ba42a88c0c1e7d4765df5e
NirtonAfonso <nirtonfilho@hotmail.com.br>
2021-09-07 16:30:26 -0300

    Initial commit

---
+++ src/br/com/pdi/vision/BasicThreshold.java
@@ -0,0 +1,47 @@
+package br.com.pdi.vision;
+
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class BasicThreshold {
+
+	BufferedImage image;
+	int width, height, red, green, blue, gray;
+	int threshold = 57;
+	
+	public void run() throws IOException {
+		File input = new File("D:\\Users\\nirto\\Documents\\GitHub\\"+
+							  "PDI\\pdi-atv-1\\src\\images\\image1.jfif");
+		image = ImageIO.read(input);
+		width = image.getWidth();
+		height = image.getHeight();
+		
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				Color pixel = new Color(image.getRGB(j, i));
+				
+				red = pixel.getRed();
+				green = pixel.darker().darker().darker().getGreen();
+				//blue = pixel.darker().getBlue();
+				gray = (red + green)/3;
+				if(gray > threshold) {
+					image.setRGB(j, i, Color.WHITE.getRGB());
+				}else {
+					image.setRGB(j, i, Color.BLACK.getRGB());
+					
+				}
+			}
+		}
+		
+		File output = new File("D:\\Users\\nirto\\Documents\\GitHub\\"+
+							  "PDI\\pdi-atv-1\\src\\images\\image1_bw.jpg");
+		ImageIO.write(image, "jpg", output);
+		
+		System.out.println("Done!");
+		
+	}
+}
---
+++ src/br/com/pdi/vision/Main.java
@@ -0,0 +1,17 @@
+package br.com.pdi.vision;
+
+import java.io.IOException;
+
+public class Main {
+
+	public static void main(String[] args) {
+		try {
+			new BasicThreshold().run();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
